e36c2fdf7f8e0fe3ffa10f3113bbe63507a619d6
Callum R John <c.john21@imperial.ac.uk>
2025-06-12 16:29:12 +0100

    addition of custom feature generation (custom_gen) for (potentially) incompatible functionality - untested

---
+++ src/featurisation/custom_gen.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+from rdkit import Chem
+from rdkit.Chem import rdFingerprintGenerator, AllChem
+from src.utils.config import load_config
+
+'''SULFUR NUCLEOPHILES-----------------------------------------------'''
+
+def has_thiol(mol):
+    """Check if a molecule contains a thiol group (-SH)."""
+    thiol_smarts = Chem.MolFromSmarts('[SX2H]')
+    return mol.HasSubstructMatch(thiol_smarts)
+
+def has_thioether(mol):
+    """Check if a molecule contains a thioether group (R-S-R')."""
+    thioether_smarts = Chem.MolFromSmarts('[SX2;$([S][C;!$(C=O)])][C;!$(C=O)]') # does NOT include thioethers with carbonyls
+    return mol.HasSubstructMatch(thioether_smarts)
+
+def has_thioester(mol):
+    """Check if a molecule contains a thioester group (R-S-C(=O)-R')."""
+    thioester_smarts = Chem.MolFromSmarts('[C](=O)[S][#6]')
+    return mol.HasSubstructMatch(thioester_smarts)
+
+def has_thiocarboxylic_acid(mol):
+    """Check if a molecule contains a thiocarboxylic acid group (R-S-C(=S)-OH)."""
+    thiocarboxylic_acid_smarts = Chem.MolFromSmarts('[C](=O)[SX2H]')
+    return mol.HasSubstructMatch(thiocarboxylic_acid_smarts)
+
+def has_sulfoxide(mol):
+    """Check if a molecule contains a sulfoxide group (R-S(=O)-R')."""
+    sulfoxide_smarts = Chem.MolFromSmarts('[S](=O)[#6]')
+    return mol.HasSubstructMatch(sulfoxide_smarts)
+
+'''OTHER INCOMPATABLE MOIETIES?--------------------------------------'''
+
+def has_alkene(mol):
+    """Check if a molecule contains an alkene group (C=C)."""
+    alkene_smarts = Chem.MolFromSmarts('C=C')
+    return mol.HasSubstructMatch(alkene_smarts)
+
+def has_alkyne(mol):
+    """Check if a molecule contains an alkyne group (C#C)."""
+    alkyne_smarts = Chem.MolFromSmarts('C#C')
+    return mol.HasSubstructMatch(alkyne_smarts)
+
+def has_tertiary_alcohol(mol):
+    """Check if a molecule contains a tertiary alcohol group."""
+    tertiary_alcohol_smarts = Chem.MolFromSmarts('[CX4](O)([CX4])([CX4])[CX4]')
+    return mol.HasSubstructMatch(tertiary_alcohol_smarts)
+
+def has_glycosamine(mol):
+    """Check if a molecule contains a glycosamine group."""
+    glycosamine_smarts = Chem.MolFromSmarts('[C;!$(C=O)]([O;!$(C=O)])([N;!$(C=O)])')
+    return mol.HasSubstructMatch(glycosamine_smarts)
+
+def has_glycoside(mol):
+    """Check if a molecule contains a glycoside group."""
+    glycoside_smarts = Chem.MolFromSmarts('[C;!$(C=O)]([O;!$(C=O)])([O;!$(C=O)])')
+    return mol.HasSubstructMatch(glycoside_smarts)
+
+
