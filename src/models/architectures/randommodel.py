79a7017ca29dc751f0bbfe6334fb5f32e59237a4
Callum R John <c.john21@imperial.ac.uk>
2025-06-18 17:31:23 +0100

    addition of a randomised dummy model and validation

---
+++ src/models/architectures/randommodel.py
@@ -0,0 +1,25 @@
+from sklearn.base import BaseEstimator, RegressorMixin
+import numpy as np
+
+class RANDOMModel(BaseEstimator, RegressorMixin):
+    def __init__(self, **kwargs):
+        pass
+
+    def fit(self, X, y):
+        self.y_min = np.min(y)
+        self.y_max = np.max(y)
+        return self
+    
+    def train(self, X, y):
+        self.fit(X, y)
+
+    def predict(self, X):
+        return np.random.uniform(self.y_min, self.y_max, size=len(X))
+
+    def save(self, path):
+        import joblib
+        joblib.dump(self.model, path)
+
+    def load(self, path):
+        import joblib
+        self.model = joblib.load(path)
